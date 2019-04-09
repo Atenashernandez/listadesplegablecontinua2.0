0ff7fcc8aea738eb1d6c193a85ab01512555a225
Tesoem <Tesoem@ISC-Tesoem-iMac.local>
2019-04-09 13:23:19 -0500

    Initial commit

---
+++ app/src/main/java/listadesplegablecontinua/edu/tesoem/itics/atenas/listadesplegablecontinua/listadesplegable.java
@@ -0,0 +1,13 @@
+package listadesplegablecontinua.edu.tesoem.itics.atenas.listadesplegablecontinua;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class listadesplegable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listadesplegable);
+    }
+}
